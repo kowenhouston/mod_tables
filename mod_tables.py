c371e4d4ccaa047921a7221f8d2e27e63a9c8a9f
Kowen Houston <kowen.houston@gmail.com>
2020-05-25 11:41:45 +0100

    first

---
+++ mod_tables.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from markupsafe import escape
+from flask import request
+
+import shlex, subprocess
+app = Flask(__name__)
+
+ALLOW_VNC_COMMAND = "iptables -I INPUT -p tcp -s <IP> --dport 5901 -j ACCEPT"
+ALLOW_SSH_COMMAND = "iptables -I INPUT -p tcp -s <IP> --dport 22 -j ACCEPT"
+
+def run_commands(ip):
+    command = ALLOW_SSH_COMMAND.replace('<IP>', escape(ip))
+    command = command.split()
+    subprocess.call(command)
+    return f'{ip} allowed'
+
+@app.route('/ip/add/<ip>')
+def add_ip(ip):
+    return run_commands(escape(ip))
+
+@app.route('/ip/add/', methods=['GET'])
+def add_my_ip():
+    return run_commands(escape(request.remote_addr))
---
+++ start.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=mod_tables.py
+flask run
